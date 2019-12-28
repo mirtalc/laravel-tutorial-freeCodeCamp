0aa5c6536904b10ae2f42947c6bbae9f81dfc049
mirtalc <mirta.lopez.cardona@gmail.com>
2019-12-28 13:56:56 +0100

    feat(#9): Editar ProfilesController per a passar variables a la vista

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    //
-    public function index()
+    // Rep com a argument la variable de la ruta a web.php
+    public function index($userId)
     {
-        return view('home');
+        // Obtenim usuari a través de la ID
+        $user = User::find($userId);
+
+        // Cridem a la vista, passant-li array de variables
+        return view('home', [
+            'user' => $user,
+        ]);
     }
 }
