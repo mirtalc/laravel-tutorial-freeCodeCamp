aad44fca46ee437375e35a29cd2b52b93e97cf00
mirtalc <mirta.lopez.cardona@gmail.com>
2019-12-29 14:22:11 +0100

    feat(#14): Permitir crear post a usuaris loguejats a partir de la Relationship de FK

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('posts/create');
@@ -25,6 +30,6 @@ class PostsController extends Controller
         i no el passaria al create().
         Aleshores, si tenim un camp que no requerix validació però volem que es guarde,
         s'especificaria com l'exemple de 'field-valid-sempre' */
-        \App\Post::create($data);
+        auth()->user()->posts()->create($data);
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $guarded = [];
+
     /**
      * Relationship per a l'usuari
      */
